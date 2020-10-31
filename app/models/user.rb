8092cef2a80bffe293286a76c3256c6f3bfa30bc
AmyFong82 <amyhfong@gmail.com>
2020-10-31 00:07:34 -0400

    initialize User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	has_many :bills
+
+	def initialize(params[:username])
+		if !User.all.any? { |u| u.username == username }
+			User.create(username: params[:username], email: params[:email], password: params[:password])
+		else
+			return
+		end
+	end
 	
 end
