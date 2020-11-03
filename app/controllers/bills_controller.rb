3a7a3daf7261e04c2a389fca2e643c6272b58943
AmyFong82 <amyhfong@gmail.com>
2020-11-03 00:10:31 -0500

    added get /bills route

---
+++ app/controllers/bills_controller.rb
@@ -0,0 +1,14 @@
+class BillsController < ApplicationController
+	get '/bills' do
+		if session[:user_id]
+	        @user = User.find(session[:user_id])
+	        @bills = @user.bills
+			erb :"/bills/bills"
+		else
+			redirect "login"
+		end
+	end
+
+end
+
+#bill = Bill.new(company_name: "Coned", amount: 100.50, due_date: "Nov20_2020", paid: false, user_id: 1)
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+
+	get '/signup' do
+		erb :"/users/create_user"
+	end
+
+	post '/signup' do
+		if !params.values.any? ("")
+			@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+			if @user.validate(params)
+				@user.save
+				session[:user_id] = @user.id 
+				redirect "/bills"
+			else
+				"Please use another username"
+			end
+		else
+			"Please fill in all the fields!"
+		end
+	end
+
+	get '/login' do
+		erb :"users/login"
+	end
+
+	post '/login' do
+		@user = User.find_by(username: params[:username])
+		
+		if @user && @user.authenticate(params[:password])
+			session[:user_id] = @user.id 
+			redirect '/bills'
+		else
+			redirect '/login'
+			"flash message"
+		end
+
+	end
+	
+end
