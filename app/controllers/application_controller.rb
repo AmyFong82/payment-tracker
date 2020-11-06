5e51491308abfc19b5de427771da9c3724d50e24
AmyFong82 <amyhfong@gmail.com>
2020-11-05 22:58:41 -0500

    working on bills/new.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,11 @@ class ApplicationController < Sinatra::Base
 
 
   get '/' do
-  	erb :'index'
+  	if session[:user_id]
+  		redirect "/bills"
+  	else
+  		erb :'index'
+  	end
   end
 
   helpers do
